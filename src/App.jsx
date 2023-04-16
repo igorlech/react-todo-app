8ff7b94a4ceed168a9064dab2a516f04ba55a0a0
igorlech <eaaigle@students.eaaa.dk>
2023-04-16 16:32:02 +0200

    added JSX markup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
 import "./styles.css";
 
 export default function App() {
-  return "App is running!";
+  return (
+    <section>
+      <form className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input type="text" id="item" />
+        </div>
+        <botton className="btn">Add</botton>
+      </form>
+      <h1 className="header">Todo List</h1>
+      <ul className="list">
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item 1
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+      </ul>
+    </section>
+  );
 }
