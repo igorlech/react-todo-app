3cbcec18226e8206951120f04ab1f38c894056f9
igorlech <eaaigle@students.eaaa.dk>
2023-04-18 17:36:35 +0200

    added initial form handler for todos

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,25 @@ import "./styles.css";
 
 export default function App() {
   const [newItem, setNewItem] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(newItem);
+
+    setTodos((currentTodos) => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), title: newItem, completed: false },
+      ];
+    });
+  }
+
+  console.log(todos);
+
   return (
     <section>
-      <form className="new-item-form">
+      <form onSubmit={handleSubmit} className="new-item-form">
         <div className="form-row">
           <label htmlFor="item">New Item</label>
           <input
@@ -15,7 +31,7 @@ export default function App() {
             id="item"
           />
         </div>
-        <botton className="btn">Add</botton>
+        <button className="btn">Add</button>
       </form>
       <h1 className="header">Todo List</h1>
       <ul className="list">
